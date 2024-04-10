1d9db3e72bb61ff6e25d55bad7142df18ab27af5
Pistipisti94 <pistipisti57@gmail.com>
2024-04-10 10:13:14 +0200

    Nevek kiírásaa

--- Vizsga_app/Program.cs
+++ Vizsga_app/Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Vizsga_app;
 
 namespace Vizsga_app
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static async Task Main(string[] args)
         {
-            asd
+            await UgyfelAdatok();
+            Console.Title = "Ugyfelek";
+
+
+        }
+        private static async Task UgyfelAdatok()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage responseMessage = await httpClient.GetAsync("http://localhost/Vizsga_Backend/index.php?ugyfelek");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string jsonString = await responseMessage.Content.ReadAsStringAsync();
+                var ugyfelek = Ugyfelek.FromJson(jsonString);
+                foreach (var item in ugyfelek)
+                {
+                    Console.WriteLine($"{item.Nev}");
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
